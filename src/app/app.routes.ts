e55d759d57db1132213770d36500f00a10612ec3
radigirl <radigirl@abv.bg>
2026-04-01 10:06:31 +0300

    initial portfolio setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Certificates } from './features/certificates/certificates';
+import { Contact } from './features/contact/contact';
+import { NotFound } from './features/not-found/not-found/not-found';
+import { ProjectDetail } from './features/project-detail/project-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'projects/:slug', component: ProjectDetail },
+  { path: 'certificates', component: Certificates },
+  { path: 'contact', component: Contact },
+  { path: '**', component: NotFound }
+];
---
+++ src/app/features/certificates/certificates.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificates',
+  imports: [],
+  templateUrl: './certificates.html',
+  styleUrl: './certificates.scss',
+})
+export class Certificates {}
